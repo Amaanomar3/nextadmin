3d8d5eaec33451ac4e709e07ef2278b0fddca316
Amaan Omar <amaanomar3@gmail.com>
2024-04-16 19:46:37 +0200

    Update form placeholders and styles in addProduct module

--- app/dashboard/products/add/page.jsx
+++ app/dashboard/products/add/page.jsx
@@ -4,17 +4,17 @@ const AddProductPage = () => {
   return (
     <div className={styles.container}>
       <form className={styles.form}>
-        <input type="text" placeholder="title" name="title" required />
+        <input type="text" placeholder="Title" name="title" required />
         <select name="cat" id="cat">
           <option value="general">Choose a Catagory</option>
           <option value="kitchen">Kitchen</option>
           <option value="kitchen">Phone</option>
           <option value="kitchen">Computer</option>
         </select>
-        <input type="number" placeholder="price" name="price" />
-        <input type="number" placeholder="stock" name="stock" />
-        <input type="text" placeholder="color" name="color" />
-        <input type="text" placeholder="size" name="size" />
+        <input type="number" placeholder="Price" name="price" />
+        <input type="number" placeholder="Stock" name="stock" />
+        <input type="text" placeholder="Color" name="color" />
+        <input type="text" placeholder="Size" name="size" />
         <textarea name="desc" id="desc" rows="16"></textarea>
         <button type="submit">Submit</button>
       </form>
---
+++ app/dashboard/users/[id]/page.jsx
@@ -0,0 +1,10 @@
+import styles from "../../../ui/dashboard/users/viewUser/viewUser.module.css";
+
+const ViewUserPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}></div>
+      <div className={styles.formContainer}></div>
+    </div>
+  );
+};
